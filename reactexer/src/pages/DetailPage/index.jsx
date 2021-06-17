066e6c5bc03739833c5e2fb6d7688bb3edab5fc7
cheatsheet1999 <37787994+cheatsheet1999@users.noreply.github.com>
2021-06-16 21:07:14 -0700

    Add files via upload

---
+++ reactexer/src/pages/DetailPage/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {UserStatus, UserUpdate} from "../../Reducers/users";
+
+
+
+
+const DetailPage = () => {
+    return (
+        <div>
+            <UserUpdate/>
+        </div>
+    )
+}
+
+export default DetailPage;
