1b9ef60749f95745298c01c6f8cd24e7e6b8e439
cheatsheet1999 <37787994+cheatsheet1999@users.noreply.github.com>
2021-09-07 14:07:08 -0700

    Update app.js

--- JS-Day/AccordionEffect/app.js
+++ JS-Day/AccordionEffect/app.js
@@ -1,22 +1,3 @@
-//
-//
-// const itemBox = document.querySelectorAll('.accordion-item');
-// const itemTitle = document.querySelectorAll('.accordion-item-heading');
-//
-// for(let i = 0; i < itemTitle.length; i++) {
-//     itemTitle[i].addEventListener('click', function() {
-//         let outerBox = this.parentNode.className;
-//         console.log(outerBox)
-//         for(let i = 0; i < itemBox.length; i++) {
-//             itemBox[i].className = 'accordion-item closed'
-//         }
-//         if(outerBox === 'accordion-item closed') {
-//             this.parentNode.className = 'accordion-item open'
-//         }
-//     })
-// }
-//
-
 
 
 const itemBox = document.querySelectorAll('.accordion-item');
