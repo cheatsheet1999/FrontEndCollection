cab2c47c98ed63b73ea104061ab387d5c5842488
cheatsheet1999 <37787994+cheatsheet1999@users.noreply.github.com>
2022-08-26 11:40:46 -0700

    Delete React directory

--- React/LoginDemo/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-import {AuthContextProvider} from "./store/auth-context";
-
-
-ReactDOM.render(<AuthContextProvider> <App/> </AuthContextProvider>, document.getElementById('root'));
